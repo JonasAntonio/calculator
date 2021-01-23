7704b5aeb90acee5b98548c27b9cca6671d43e88
JonasAntonio <jayv.jonas@gmail.com>
2021-01-23 13:37:24 -0300

    componente display

--- App.js
+++ App.js
@@ -2,10 +2,15 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Button from './src/components/Button'
+import Display from './src/components/Display'
 
 export default function App() {
+  let state = {
+    displayValue: '0'
+  }
   return (
     <View style={styles.container}>
+      <Display value={state.displayValue}></Display>
       <View style={styles.buttons}>
         <Button label='AC'/>
         <Button label='/'/>
